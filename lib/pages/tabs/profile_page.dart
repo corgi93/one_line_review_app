5f202dc9feea44b88b9adf6bc3e74bcd8b24ea0b
Jin <hjjang@maxst.com>
2021-02-28 18:59:53 +0900

    project settings

---
+++ lib/pages/tabs/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('profile page'),
+    );
+  }
+}
---
+++ lib/pages/tabs/search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('saerch page'),
+    );
+  }
+}
